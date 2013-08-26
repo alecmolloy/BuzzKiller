c0861f2ec9f2920304fdaab8de5aef64572ee713
Alec Molloy <czaravm@gmail.com>
2013-08-25 23:17:08 -0700

    added comments

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ for (var i = 0; i < anchorTags.length; i++) {
     if (anchorTags[i].href.indexOf("buzzfeed.com") > -1)
     {
         anchorTags[i].href = "http://en.wikipedia.org/wiki/Special:Random";
+        // for facebook and a few other sites, which fake the href:
         anchorTags[i].onmouseover = "";
         anchorTags[i].onclick = "";
     }
