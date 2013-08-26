93f3c308de980b6498c802badd94fe05d9314a9e
Alec Molloy <czaravm@gmail.com>
2013-08-26 11:31:12 -0700

    upworthy support + more robust on facebook

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-var anchorTags = document.getElementsByTagName('a');
-
-for (var i = 0; i < anchorTags.length; i++) {
-    if (anchorTags[i].href.indexOf("buzzfeed.com") > -1)
-    {
-        anchorTags[i].href = "http://en.wikipedia.org/wiki/Special:Random";
-        // for facebook and a few other sites, which fake the href:
-        anchorTags[i].onmouseover = "";
-        anchorTags[i].onclick = "";
+setInterval(function(){
+    var anchorTags = document.getElementsByTagName('a');
+    
+    for (var i = 0; i < anchorTags.length; i++) {
+        if ((anchorTags[i].href.indexOf("buzzfeed.com") > -1) || (anchorTags[i].href.indexOf("upworthy.com") > -1))
+        {
+            anchorTags[i].href = "http://en.wikipedia.org/wiki/Special:Random";
+            // for facebook and a few other sites, which fake the href:
+            anchorTags[i].onmouseover = null;
+            anchorTags[i].onclick = null;
+            anchorTags[i].removeAttribute("onclick");
+            anchorTags[i].removeAttribute("onmouseover");
+        }
     }
-}
-
+},150);
